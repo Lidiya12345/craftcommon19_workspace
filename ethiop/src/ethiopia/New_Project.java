7892ef6f153f097c54f54dbcd48d825dadfa426e
lidiya t <lidiyates@outlook.com>
2023-05-26 17:04:43 -0400

    new commit

--- ethiop/src/ethiopia/New_Project.java
+++ ethiop/src/ethiopia/New_Project.java
@@ -5,6 +5,8 @@ public class New_Project {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(" i love pizza");
+System.out.println(" i love pizza");
+System.out.println(" i love pizza");
 	}
 
 }
